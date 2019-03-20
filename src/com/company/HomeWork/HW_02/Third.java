26b922ecc0c57cc71b74083753a747ad8c7be849
Deni1202 <deni120287@gmail.com>
2019-03-20 20:54:43 +0200

    commit

--- src/com/company/HomeWork/HW_02/Third.java
+++ src/com/company/HomeWork/HW_02/Third.java
@@ -17,8 +17,12 @@ public class Third {
 
         if(a<b && b<c){
             System.out.println(b);
-        }else{
+        }else if (a<c && c<b){
             System.out.println(c);
+        }else if(c<a && a<b){
+            System.out.println(a);
+        }else if(c<b && b<a){
+            System.out.println(b);
         }
 
     }
