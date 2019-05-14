79fc623e04cbae05dc58ced1a48863a9f3a8b0bf
Admin <deni120287@gmail.com>
2019-05-14 20:34:26 +0300

    commit

--- src/com/company/lesson_17/Task_1701.java
+++ src/com/company/lesson_17/Task_1701.java
@@ -1,6 +1,7 @@
 package com.company.lesson_17;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +20,9 @@ import java.util.List;
 */
 public class Task_1701 {
     public static void main(String[] args){
-
+    System.out.println(allString(stringOut()));
     }
-    private static List<String>allString(){
+    private static List<String>allString() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         List<String>allString=new ArrayList<>();
         String a = "";
@@ -34,10 +35,16 @@ public class Task_1701 {
         return allString;
     }
     private static void stringOut(List<String>allString){
+        List<String>newList = new ArrayList<>();
         for(int i = 0; i<allString.size();i++) {
             if (allString.size() % 2 == 0) {
-                System.out.println()
+              for(int a = 0;a<=1;a++){
+                  newList.add(0,allString.get(i));
+              }
             } if (allString.size() % 2 != 0) {
+                for(int a = 0;a<=2;a++){
+                    newList.add(0,allString.get(i));
+                }
 
             }
         }
