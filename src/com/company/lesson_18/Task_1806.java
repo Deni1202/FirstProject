4f7a780f4defee30cf66b85aadaa2f644cc662eb
Admin <deni120287@gmail.com>
2019-05-20 22:19:07 +0300

    commit

--- src/com/company/lesson_18/Task_1806.java
+++ src/com/company/lesson_18/Task_1806.java
@@ -7,12 +7,7 @@ import java.util.Date;
 public class Task_1806 {
     public static void main(String[] args){
         Date dateNow = new Date();
-        dateNow.setMinutes(0);
-        dateNow.setHours(0);
-        Date dayBegin = new Date();
-        long time = dayBegin.getTime() - dateNow.getTime();
-        long timeInAll = 60*60*1000;
-        long result = time/timeInAll;
-        System.out.println(result);
+
+        System.out.print("Секунд:" + dateNow.getSeconds() +" "+ "Минут: " + dateNow.getMinutes() +" " + "Часов: " + dateNow.getHours());
     }
 }
