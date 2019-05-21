fb5de3333ba3a007cbc27df7b079931582cb69f9
Admin <deni120287@gmail.com>
2019-05-21 21:04:34 +0300

    commit

--- src/com/company/lesson_19/Task_1901.java
+++ src/com/company/lesson_19/Task_1901.java
@@ -5,21 +5,38 @@ package com.company.lesson_19;
 */
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Task_1901 {
-    public static void main(String[] args){
-       Date dateBegin = new Date();
-       List list = new LinkedList();
-       for(int i = 0; i<10000;i++){
-           list.add(i);
-       }
-       Date dateInTheEnd = new Date();
-       long time = dateInTheEnd.getTime() - dateBegin.getTime();
-        System.out.println(dateBegin.getTime());
-        System.out.println(dateInTheEnd.getTime());
-       System.out.println(time);
+    public static void main(String[] args) throws IOException {
+
+     getTime();
     }
+    private static long getTime() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Date dateBegin = new Date();
+        int c = Integer.parseInt(reader.readLine());
+        if(c=0){
+        List list = new LinkedList();
+        for(int i = 0; i<10000;i++) {
+            list.add(5000, i);
+        }
+            if (c == 1) {
+                List list = new ArrayList();
+                for(int i = 0; i<10000;i++) {
+                    list.add(5000, i);
+                }
+            }
+        }
+        Date dateInTheEnd = new Date();
+        long time = dateInTheEnd.getTime() - dateBegin.getTime();
+        return time;
+    }
+
 }
