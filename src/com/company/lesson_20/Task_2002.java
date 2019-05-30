fcc852c7cf80b8d3d1bc81c35308dba63e598a86
Admin <deni120287@gmail.com>
2019-05-30 21:03:37 +0300

    commit

---
+++ src/com/company/lesson_20/Task_2002.java
@@ -0,0 +1,28 @@
+package com.company.lesson_20;
+
+/* Вывести на экран список ключей
+Есть коллекция HashMap<String, String>, туда занесли 3 различные пары.
+При помощи метода printKeys:
+Вывести на экран список ключей, каждый элемент с новой строки.
+*/
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Task_2002 {
+    public static void maine(String[] args){
+    Map<String,String> names = new HashMap<>();
+    names.put("first", "I am");
+    names.put("second", "I will");
+    names.put("third", "I was");
+        printKeys();
+    }
+    public static void printKeys(Map<String,String>names){
+        Iterator<Map.Entry<String,String>>allNames = map.entrySet().iterator();
+        while(allNames.hasNext()){
+            Map.Entry<String,String> text = allNames.next();
+            System.out.println(text.getKey() + " " + text.getValue());
+        }
+    }
+}
