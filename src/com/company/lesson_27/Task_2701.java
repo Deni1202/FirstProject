bcc4b73670f3a997652cea750b48f89c38b6e7e8
Admin <deni120287@gmail.com>
2019-06-21 07:43:11 +0300

    commit

--- src/com/company/lesson_27/Task_2701.java
+++ src/com/company/lesson_27/Task_2701.java
@@ -6,46 +6,45 @@ package com.company.lesson_27;
 Вывести содержимое словаря на экран.
 В тестах регистр (большая/маленькая буква) влияет на результат.
 */
-/*
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Task_2701 {
-    public static void main(String[]args){
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> result = chekRepeet(allWords());
+        System.out.println(result);
 
     }
+
     private static List<String> allWords() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         List<String> words = new ArrayList<>();
-        for(int i = 0 ;i<5;i++){
+        for (int i = 0; i < 5; i++) {
             words.add(reader.readLine());
         }
         return words;
     }
-    private static void chekRepeet(List<String>words){
-        Map<String,Integer> chekWords= new HashMap<>();
-        for(int i =0;i<words.size();i++){
-            chekWords.put(words.get(i),0);
-        }
-        Iterator<Map.Entry<String,Integer>> allWords = chekWords.entrySet().iterator();
-        while(allWords.hasNext()){
 
+    private static Map<String, Integer> chekRepeet(List<String> words) {
+        Map<String, Integer> chekWords = new HashMap<>();
+        for (int i = 0; i < words.size(); i++) {
+            chekWords.put(words.get(i), 0);
         }
-
-
-        }
-    private static List<String> chekedWords(List<String>words){
-        int count = 0;
-        for(int i = 0;i<words.size();i++){
-            if(words.get(i).equals(words.get(i+1))){
-              count++;
+        Iterator<Map.Entry<String, Integer>> allWords = chekWords.entrySet().iterator();
+        while (allWords.hasNext()) {
+            Map.Entry<String, Integer> result = allWords.next();
+            for (int a = 0; a < words.size(); a++) {
+                if (result.getKey().equals(words.get(a))) {
+                    chekWords.put(words.get(a), result.getValue() + 1);
+                }
             }
+
         }
 
+        return chekWords;
     }
 }
 
-
-*/
