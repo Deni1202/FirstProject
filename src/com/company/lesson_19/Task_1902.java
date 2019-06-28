411c2b5818780ed1452eaf8a8c0e189c771ef346
Admin <deni120287@gmail.com>
2019-06-28 14:05:05 +0300

    commit

--- src/com/company/lesson_19/Task_1902.java
+++ src/com/company/lesson_19/Task_1902.java
@@ -10,7 +10,6 @@ import com.sun.xml.internal.bind.v2.TODO;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//TODO: 23.06.2019
 
 public class Task_1902 {
     public static void main(String[] args) throws IOException {
@@ -27,11 +26,11 @@ chekWords(allWords());
     }
     private static void chekWords(String[] words) {
         for (int i = 0; i < words.length; i++) {
-            for (int a = i; a < words.length; a++) {
-                if (words[a].compareTo(words[i]) < 0) {
+            for (int a = 0; a < words.length-1; a++) {
+                if (words[a].compareTo(words[a+1]) > 0) {
                     String n = words[a];
-                    words[a] = words[i];
-                    words[i] = n;
+                    words[a] = words[a+1];
+                    words[a+1] = n;
                 }
             }
             System.out.println(words[i]);
